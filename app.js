2ea997e1a32d15b9b45c19f0a36a7dbe31e0e8e5
Tomasz Pietraszek <tomekpietraszek@gmail.com>
2024-01-30 17:26:21 +0100

    cors middlewareadded

--- app.js
+++ app.js
@@ -15,12 +15,25 @@ const app = express();
 
 const { PORT = 5000, DB_URI } = process.env;
 
+app;
 app
+  .use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://simple-full-stack-project.onrender.com"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    next();
+  })
   .use(
     cors({
       origin: [
         "http://localhost:5173",
-        "https://simple-full-stack-project.onrender.com "
+        "https://simple-full-stack-project.onrender.com ",
       ],
       credentials: true,
     })
